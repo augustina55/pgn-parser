08ca40ee6d199dc5ee3f8a45369720a4b52edaa6
Augustin A <79752571+augustina55@users.noreply.github.com>
2026-03-14 11:57:31 +0530

    Update page.tsx
    included remove pawns from FEN

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 import { PgnDetailer } from "@/components/PgnDetailer"
+import { FenCleaner } from "@/components/FenCleaner"
 
 export default function Home() {
-  return (
-    <main className="max-w-6xl mx-auto p-8">
-      <PgnDetailer />
-    </main>
-  )
+
+return (
+
+<div className="max-w-6xl mx-auto p-10 space-y-12">
+
+<PgnDetailer/>
+
+<FenCleaner/>
+
+</div>
+
+)
+
 }
